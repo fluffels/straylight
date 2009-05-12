db46bc153b6de5006e0a291a15f7e6ae7ac75881
Jan Kroeze <jan@monolith>
2009-05-12 15:27:48 +0200

    Code to determine the intersection of a sphere and a ray.

--- Sphere.C
+++ Sphere.C
@@ -18,8 +18,8 @@ getNormalAt(const Vector3<GLdouble>& p) const
 {
    /* See p. 19 of 3D Computer Graphics by Alan Watt (3rd Edition). */
    Vector3<GLdouble> n((p.getX() - _pos.getX()) / _radius,
-                      (p.getY() - _pos.getY()) / _radius,
-                      (p.getZ() - _pos.getZ()) / _radius);
+                       (p.getY() - _pos.getY()) / _radius,
+                       (p.getZ() - _pos.getZ()) / _radius);
 
    return n.normalise();
 }
@@ -28,4 +28,35 @@ void Sphere::
 setMaterial(Material& newMaterial)
 {
    _mat = newMaterial;
+}
+
+bool Sphere::
+testIntersection(Ray& r, Vector3<GLdouble>& p)
+{
+   /* See p. 18 of 3D Computer Graphics by Alan Watt, 3rd Edition. */
+   
+   /* Note that the Ray's direction vector is guaranteed to be a unit vector,
+    * as the class normalises the vector upon receiving it. */
+    
+   Vector3<GLdouble> distance = r.getPos() - this->_pos;
+   
+   GLdouble b = (distance * 2).dot(r.getDir()); 
+   GLdouble c = distance.dot(distance) - pow(this->_radius, 2);
+   GLdouble delta = pow(b, 2) - c;
+   
+   if (delta < 0)
+   {
+      return false;
+   }
+   else
+   {
+      GLdouble t = -b - sqrt(delta);
+      if (t < 0)
+      {
+         t = -b + sqrt(delta);
+      }
+      
+      p = r.getPos() + r.getDir() * t;
+      return true;      
+   }
 }
--- Sphere.h
+++ Sphere.h
@@ -1,11 +1,14 @@
 #ifndef SPHERE_H_
 #define SPHERE_H_
 
+#include <cmath>
+
 #include <GL/glut.h>
 
 #include "GraphicsLibrary/Vector3.h"
 
 #include "Material.h"
+#include "Ray.h"
 
 /**
  * Models a spherical object using a centre point and a radius. Also has
@@ -24,6 +27,14 @@ class Sphere
       Vector3<GLdouble> getNormalAt(const Vector3<GLdouble>& p) const;
       
       void setMaterial(Material& newMaterial);
+      
+      /**
+       * Tests for intersection between this Sphere and a given Ray.
+       * @param r The Ray to test against.
+       * @param p The intersection point is stored in this variable.
+       * @return True if the Ray intersects this Sphere, false otherwise.
+       */
+      bool testIntersection(Ray& r, Vector3<GLdouble>& p);
       
    private:
       Vector3<GLdouble> _pos;
