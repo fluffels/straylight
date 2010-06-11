081826c7127db5c4b109f063f7142a1270de17a4
Jan Kroeze <jan@straylight>
2010-06-11 15:03:28 +0200

    * Comments and formatting for KB-spline rotation code.

--- AFF/kbsplrot.h
+++ AFF/kbsplrot.h
@@ -1,5 +1,20 @@
-/* this code originates from www.magic-software.com */
-/* thanks to Dave Eberly for letting us use this */
+/**
+ * This code originates from www.magic-software.com thanks to Dave 
+ * Eberly for letting us use this.
+ * 
+ * Kochanek-Bartels tension-continuity-bias spline interpolation for
+ * rotational data.
+ * 
+ * (K-B Spline Rotation Functions).
+ * 
+ * Some modification (mostly comments by Jan Kroeze, 2010).
+ * 
+ * The implementation here is a little different from the one discussed
+ * at http://en.wikipedia.org/wiki/Kochanek-Bartels_spline because there
+ * is an adjustment factor for each tangent and each knot has its own
+ * tension, bias and continuity value.
+ */
+
 #ifndef KBSPLROT_H
 #define KBSPLROT_H
 
@@ -7,28 +22,56 @@
  * to quaternion interpolation.
  */
 
+/**
+ * Rotation data.
+ */
 typedef struct
 {
     double x, y, z;
     double angle;
-} Rotation3;
+}
+Rotation3;
 
-
+/**
+ * Rotation key frame.
+ */
 typedef struct
 {
-   /* sample time */
+   /* Sample time. */
    double t;
    
-   /* sample rotation axis and angle */
+   /* Sample rotation axis and angle. */
    Rotation3 Rot;
    
-   /* sample parameters for affecting tangential behavior at control point */
+   /* Sample parameters for affecting tangential behavior at control
+    * point. */
    double tension, continuity, bias;
-} RotationKey;
+}
+RotationKey;
 
+/**
+ * Initialize KB-spline data. Allocates memory for help structures.
+ * 
+ * @param numKeys The number of keyframes in the sequence.
+ * @param key The sequence of keyframes.
+ * @return The SplineInfo data structure as a void*.
+ */
+void* KB_RotInitialize(int numKeys, RotationKey* key);
 
-void* KB_RotInitialize  (int numKeys, RotationKey* key);
-void  KB_RotTerminate   (void* info);
-void  KB_RotInterpolate (void* info, double t, Rotation3* Rot);
+/**
+ * Deallocates memory.
+ * 
+ * @param info Pointer to the SplineInfo data structure.
+ */
+void KB_RotTerminate(void* info);
+
+/**
+ * Interpolate KB-spline data to find a rotation.
+ * 
+ * @param info The KB-spline information.
+ * @param t The time at which to find the rotation.
+ * @param Rot The rotation found after interpolation.
+ */
+void KB_RotInterpolate(void* info, double t, Rotation3* Rot);
 
 #endif
