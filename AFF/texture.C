61f8acee31fbe27200f0f0c2de104b843d1b1bfd
Jan Kroeze <jan@straylight>
2010-06-11 23:10:56 +0200

    * Comments / styling for AFF texture file.

--- AFF/texture.C
+++ AFF/texture.C
@@ -1,107 +1,129 @@
-/*----------------------------------------------------------------------
-  File:    texture.c
-  Purpose: Stuff concerning textures
-           reads PPM files (only in P6 format, i.e., RGB binary)
-----------------------------------------------------------------------*/
+/**
+ * Stuff concerning textures.
+ * 
+ * Reads PPM files (only in P6 format, i.e., RGB binary).
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "texture.h"
 
-/* skip all rows that start with #, i.e., comments */
+/**
+ * Skips all rows that start with #, i.e., comments.
+ * 
+ * @param f The file to eat comments from.
+ */
 static void eatComments(FILE *f)
 {
    int ch;
-   while((ch=getc(f))=='#')
+   while((ch = getc(f)) == '#')
    {
       char str[1000];
-      fgets(str,1000,f);
+      fgets(str, 1000, f);
    }
-   ungetc(ch,f);   
+   ungetc(ch, f);   
 }
 
+/**
+ * Skips whitespace (spaces, tabs, newlines, carriage returns and form
+ * feeds).
+ * 
+ * @param f The file to eat whitespace from.
+ */
 static void eatWhitespace(FILE *f)
 {
    int ch;
-   ch=getc(f);
-   while(ch==' ' || ch=='\t' || ch=='\n' || ch=='\f' || ch=='\r')
+   ch = getc(f);
+   while(ch == ' ' || ch == '\t' || ch == '\n' || ch == '\f'
+      || ch == '\r')
    {
-      ch=getc(f);
+      ch = getc(f);
    }
-   ungetc(ch,f);
+   ungetc(ch, f);
 }
 
-/* reads a ppm file with the P6 header (means raw RGB), returns a struct Texture */
-/* the header looks like this:
- *---------
- * P6
- * # comments if you want to
- * width height
- * 255
- *---------
- * then follows RGBRGBRGBRGBRGB...
- */
 struct Texture *viReadPPM(char *filename)
 {
    FILE *f;
-   char ch;
-   int width, height, colres;
-   if(f=fopen(filename,"rb"))
+   if(f = fopen(filename, "rb"))
    {
-      char str[100];
-      struct Texture *tx;
       eatWhitespace(f);
       eatComments(f);
       eatWhitespace(f);
-      fscanf(f,"%s",str);
-      if(strcmp(str,"P6")!=0)
-      {
-	 printf("Error: the texture image file must be of raw color PPM format,\n");
-	 printf("i.e., it must have P6 in the header. File: %s\n",filename);
-	 exit(1);
-      }
-      eatWhitespace(f);
-      eatComments(f);
-      eatWhitespace(f);
-      fscanf(f,"%d %d",&width,&height);
-      if(width<=0 || height<=0)
-      {
-	 printf("Error: width and height of the image must be greater than zero. File: %s\n",filename);
-	 exit(1);
-      }
-      eatWhitespace(f);
-      eatComments(f);
-      eatWhitespace(f);
-      fscanf(f,"%d",&colres);
-      if(colres!=255)
-      {
-	 printf("Error: color resolution must be 255.File: %s\n",filename);
-	 return NULL;
-      }
-      /* gotta eat the newline too */
-      ch=0;
-      while(ch!='\n') fscanf(f,"%c",&ch);
       
-      tx=(struct Texture*)malloc(sizeof(struct Texture));
-      if(tx==NULL)
+      char str[100];
+      fscanf(f, "%s", str);
+                 
+      if(strcmp(str, "P6") != 0)
       {
-	 printf("Error: could not allocate memory for texture struct. File: %s\n",filename);
-	 return NULL;
+         printf("Error: the texture image file must be of raw color ");
+         printf("PPM format, i.e., it must have P6 in the header. ");
+         printf("File: %s\n", filename);
+         exit(1);
       }
-      tx->mWidth=width;
-      tx->mHeight=height;
-      tx->mRGB=(unsigned char*)malloc(3*width*height);
+      
+      eatWhitespace(f);
+      eatComments(f);
+      eatWhitespace(f);
+      
+      int width, height;
+      fscanf(f, "%d %d", &width, &height);
+      if(width <= 0 || height <= 0)
+      {
+         printf("Error: width and height of the image must be ");
+         printf("greater than zero. File: %s\n", filename);
+         exit(1);
+      }
+      
+      eatWhitespace(f);
+      eatComments(f);
+      eatWhitespace(f);
+      
+      int colres;
+      fscanf(f, "%d", &colres);
+      if(colres != 255)
+      {
+         printf("Error: color resolution must be 255. ");
+         printf("File: %s\n", filename);
+         return NULL;
+      }
+      
+      /* Gotta eat the newline too. */
+      char ch;
+      ch = 0;
+      while(ch != '\n')
+      {
+         fscanf(f, "%c", &ch);
+      }
+      
+      struct Texture *tx = (struct Texture*)
+         malloc(sizeof(struct Texture));
+      
+      if(tx == NULL)
+      {
+         printf("Error: could not allocate memory for texture ");
+         printf("struct. File: %s\n", filename);
+         return NULL;
+      }
+      
+      tx->mWidth = width;
+      tx->mHeight = height;
+      tx->mRGB = (unsigned char*)malloc(3 * width * height);
       if(tx->mRGB==NULL)
       {
-	 printf("Error: could not allocate memory for the pixels of the texture. File: %s\n",filename);
-	 exit(1);
+         printf("Error: could not allocate memory for the pixels of ");
+         printf("the texture. File: %s\n", filename);
+         exit(1);
       }
       
-      if(fread(tx->mRGB,3*width*height,1,f)!=1)
+      if(fread(tx->mRGB, 3 * width * height, 1, f) != 1)
       {
-	 printf("Error: could not read 3 x %d bytes of pixel info. File: %s\n",width*height,filename);
-	 return NULL;
+         printf("Error: could not read 3 x %d bytes of pixel info. ",
+            width * height);
+         printf("File: %s\n", filename);
+         return NULL;
       }
             
       fclose(f);
@@ -110,32 +132,35 @@ struct Texture *viReadPPM(char *filename)
    }
    else
    {
-      printf("Error: could not open %s.\n",filename);
+      printf("Error: could not open %s.\n", filename);
       return NULL;
    }   
 }
 
-int viWritePPM(char *filename, int width, int height, unsigned char *pixels)
+int viWritePPM(char *filename, int width, int height,
+   unsigned char *pixels)
 {
    FILE *f;
-   if(f=fopen(filename,"wb"))
+   if(f = fopen(filename, "wb"))
    {
+      fprintf(f, "P6\n%d %d\n255\n", width, height);
+      
       int q;
-      fprintf(f,"P6\n%d %d\n255\n",width,height);
-      /* write the bottommost line first, i.e., in reversed y-order */
-      for(q=0;q<height;q++)
+      
+      /* Write the bottommost line first, i.e., in reversed y-order. */
+      for(q = 0; q < height; q++)
       {
-	 unsigned char *adr;
-	 adr=pixels+3*width*(height-1-q);
-	 fwrite(adr,3*width,1,f);
+         unsigned char *adr;
+         adr = pixels + 3 * width * (height - 1 - q);
+         fwrite(adr, 3 * width, 1, f);
       }
+      
       fclose(f);
       return 1;
    }
    else
    {
-      printf("Error: could not open file <%s>.\n",filename);
+      printf("Error: could not open file <%s>.\n", filename);
       return 0;
    }
 }
-
--- AFF/texture.h
+++ AFF/texture.h
@@ -1,20 +1,64 @@
-/*----------------------------------------------------------------------
-  File:    texture.h
-  Purpose: Stuff concerning textures
-           reads PPM files (only in P6 format, i.e., RGB binary)
-----------------------------------------------------------------------*/
+/**
+ * Stuff concerning textures.
+ * 
+ * Reads PPM files (only in P6 format, i.e., RGB binary).
+ */
 
 #ifndef TEXTURE_H
 #define TEXTURE_H
 
+/**
+ * A texture loaded from a PPM RGB binary file.
+ */
 struct Texture
 {
+   /**
+    * The width of the texture.
+    */
    int mWidth;
+   
+   /**
+    * The height of the texture.
+    */
    int mHeight;
-   unsigned char *mRGB; /* the size is 3*mWidth*mHeight */
+   
+   /**
+    * The RGB data of the texture.
+    * 
+    * The size of this memory allocation is 3 * mWidth * mHeight.
+    */
+   unsigned char* mRGB;
 };
 
-struct Texture *viReadPPM(char *filename);
-int viWritePPM(char *filename, int width, int height, unsigned char *pixels);
+/** 
+ * Reads a PPM file with the P6 header (raw RGB).
+ * 
+ * The file format looks like this:
+ * ---------
+ * P6
+ * # Comments if you want to.
+ * width height
+ * 255
+ * ---------
+ * RGBRGBRGBRGBRGB...
+ * 
+ * @param filename The name of the PPM file.
+ * 
+ * @return A texture struct containing the data read in from the file.
+ */
+struct Texture* viReadPPM(char *filename);
+
+/**
+ * Write a texture to a file.
+ * 
+ * @param filename The name of the file to write to.
+ * @param width The width of the texture to write.
+ * @param height The height of the texture to write.
+ * @param pixels The data to write.
+ * 
+ * @return
+ */
+int viWritePPM(char *filename, int width, int height,
+   unsigned char *pixels);
 
 #endif
