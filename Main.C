df3c6fe1c826b21914501601b0d2151b56a461fb
Jan Kroeze <jan@straylight>
2010-08-04 15:51:50 +0200

    Cleaned up Main.C: 	* Removed unused declarations. 	* Alphabetized functions. 	* Moved forward declarations, includes and global variables to Main.h, like the rest of the code.

--- Main.C
+++ Main.C
@@ -1,119 +1,5 @@
-#include <algorithm>
-#include <cstdlib>
-#include <cstdio>
-#include <cstring>
-#include <iostream>
-#include <map>
+#include "Main.h"
 
-#include <pthread.h>
-#include <png.h>
-
-#include "AFF/parse.h"
-
-#include "Camera.h"
-#include "Colour.h"
-#include "Light.h"
-#include "Material.h"
-#include "Plane.h"
-#include "Scene.h"
-#include "Sphere.h"
-#include "Vector.h"
-
-using namespace std;
-
-/** The center of projection. */
-const Vector COP(0.0, 1.0, 4.0);
-/** The "at" vector - the direction the camera is looking in. */
-const Vector AT(0.0, 2.0, 0.0);
-/** The "up" vector - the direction of "up" from the camera. */
-const Vector UP(0.0, 1.0, 0.0);
-/** The position of the red sphere. */
-const Vector RED_SPHERE_POS(-1.0, 1.0, 0.0);
-/** The position of the green sphere. */
-const Vector GREEN_SPHERE_POS(0.0, 1.2, -1.0);
-/** The position of the blue sphere. */
-const Vector BLUE_SPHERE_POS(1.0, 1.0, 0.0);
-/** The radius for the spheres. */
-const double SPHERE_RADIUS = 0.5;
-/** The normal to the plane. */
-const Vector PLANE_NORMAL(0.1, 1.0, 0.0);
-/** */
-//TODO: Add something here.
-static const double PLANE_D = 0.0;
-/** The scene. */
-Scene* scene;
-
-/** Title of the window. */
-const char* title = "Straylight";
-/** Whether to only cast rays and produce no input. */
-bool noOutput = false;
-/** Whether rendering is done. */
-bool renderingDone = false;
-/** Whether to parse an NFF file. */
-bool parseNFF = false;
-/** The name of the NFF file to parse. */
-char nffFileName[255] = "";
-/** Width of the image to produce. */
-int width = 0;
-/** Height of the image to produce. */
-int height = width;
-/** Amount of threads to spin up. */
-int threadCount = 4;
-
-/** Name of the output file. */
-const char* outFileName = "out.png";
-/** Output file handle. */
-FILE* outFile;
-
-/** Output file bit depth. */
-const int BIT_DEPTH = 8;
-/** Colour type of the output file. */
-const int COLOUR_TYPE = PNG_COLOR_TYPE_RGB;
-/** Interlacing to use for the output file. */
-const int INTERLACE_TYPE = PNG_INTERLACE_NONE; //_ADAM7;
-/** Compression type to use for the output file. */
-const int COMPRESSION_TYPE = PNG_COMPRESSION_TYPE_DEFAULT;
-/** Filtering method to use for the output file. */
-const int FILTER_METHOD = PNG_FILTER_TYPE_DEFAULT;
-/** Colours per pixel in the image. */
-const int COLOURS_PER_PIXEL = 3;
-/** Image data, stored as columns of pixels with three eight-bit
- * values. */
-unsigned char* image;
-
-/** libPNG master structure. */
-png_structp png;
-/** libPNG info structure. */
-png_infop pngInfo;
-
-void
-castRays();
-void*
-castRaySubset(void* args);
-void
-loadNFFFile();
-void
-loadStaticScene();
-void
-printUsage();
-Colour
-shootRay(Ray& r);
-bool
-shootShadowRay(Ray& r);
-void
-teardown();
-void
-writeImage();
-
-/**
- * The main loop for the program.
- * 
- * Casts rays in turns and writes them to the image buffer and
- * optionally to the screen.
- * 
- * @param interactive If true, the image will be output to the
- * screen while it is being calculated.
- */
 void
 castRays()
 {
@@ -148,19 +34,6 @@ castRays()
    delete[] coeffs;
 }
 
-/**
- * Casts a subset of rays. Used to divide the scene into equal parts for
- * threading.
- * 
- * @param arg A single argument that determines which segment of the
- * scene to render in this thread.
- * 
- * The scene is divided into threadCount y-parts: cutoff = height
- * / threadCount, and then the arg is used to determine the lower and
- * upper bounds: low = arg * cutoff, high = (arg + 1) * cutoff.
- * 
- * Therefore: arg e N [0;threadCount)
- */
 void*
 castRaySubset(void* arg)
 {
@@ -190,83 +63,11 @@ castRaySubset(void* arg)
    pthread_exit(NULL);
 }
 
-/**
- * Shoots an individual ray.
- * 
- * @param r The ray to shoot.
- * @return The colour returned by the ray.
- */
-Colour
-shootRay(Ray& r)
-{
-   Colour black(0.0, 0.0, 0.0);
-
-   if (scene->testIntersection(r) == true)
-   {
-      const SceneObject* s = r.intersected;
-      const Material& m = s->mat;
-      
-      Colour localColour;
-      if (shootShadowRay(r))
-      {
-         Colour colour = scene->light.getGlobalLightAt(r, scene->cam.getCOP());
-         localColour = colour / 2;
-      }
-      else
-      {
-         localColour = scene->light.getGlobalLightAt(r, scene->cam.getCOP());
-      }
-
-      if ((r.depth != 0) && (m.reflective))
-      {
-         /* 3D Computer Graphics by Alan Watt, p. 24 */
-         Vector p = r.intersection;
-         Vector n = r.intersected->getNormalAt(p);
-         Vector l = r.dir * -1;
-         
-         Vector reflect = n * (n.dot(l) * 2) - l;
-         
-         reflect = reflect.normalise();
-         p += reflect * 0.01;
-         Ray newRay(p, reflect);
-         newRay.depth = r.depth - 1;
-
-         Colour Lri = shootRay(newRay);
-         return localColour.combine(Lri);
-      }
-      else
-      {
-         return localColour;
-      }
-   }
-   else
-   {
-      return scene->background;
-   }
-}
-
-/**
- * Shoots a ray to determine if an intersection point struck by
- * a ray is in shadow.
- * 
- * @return True if the intersection point is in shadow, false
- * otherwise.
- */
-bool
-shootShadowRay(Ray& r)
-{
-   Vector p = r.intersection;
-   Vector l = (scene->light.pos - p).normalise();
-
-   p += l * 0.01;
-   Ray shadowRay(p, l);
-
-   return scene->testIntersection(shadowRay);
-}
-
 void
 loadNFFFile()
 {
+   scene = new Scene();
+
    FILE* nffFile = fopen(nffFileName, "r");
    
    if (nffFile == NULL)
@@ -277,6 +78,24 @@ loadNFFFile()
    viParseFile(nffFile, *scene, width, height);
    
    fclose(nffFile);
+}
+
+int
+main(int argc, char** argv)
+{
+   parseArguments(argc, argv);
+
+   loadNFFFile();
+
+   setup();
+
+   castRays();
+
+   writeImage();
+
+   teardown();
+
+   return 0;
 }
 
 void
@@ -295,8 +114,6 @@ parseArguments(int argc, char** argv)
             printf("Option '--file / -f' requires an argument.\n\n");
             printUsage();
          }
-
-         parseNFF = true;
          
          strcpy(nffFileName, argv[a + 1]);
          
@@ -412,11 +229,72 @@ setup()
    png_write_info(png, pngInfo);
 }
 
+
+Colour
+shootRay(Ray& r)
+{
+   Colour black(0.0, 0.0, 0.0);
+
+   if (scene->testIntersection(r) == true)
+   {
+      const SceneObject* s = r.intersected;
+      const Material& m = s->mat;
+
+      Colour localColour;
+      if (shootShadowRay(r))
+      {
+         Colour colour = scene->light.getGlobalLightAt(r, scene->cam.getCOP());
+         localColour = colour / 2;
+      }
+      else
+      {
+         localColour = scene->light.getGlobalLightAt(r, scene->cam.getCOP());
+      }
+
+      if ((r.depth != 0) && (m.reflective))
+      {
+         /* 3D Computer Graphics by Alan Watt, p. 24 */
+         Vector p = r.intersection;
+         Vector n = r.intersected->getNormalAt(p);
+         Vector l = r.dir * -1;
+
+         Vector reflect = n * (n.dot(l) * 2) - l;
+
+         reflect = reflect.normalise();
+         p += reflect * 0.01;
+         Ray newRay(p, reflect);
+         newRay.depth = r.depth - 1;
+
+         Colour Lri = shootRay(newRay);
+         return localColour.combine(Lri);
+      }
+      else
+      {
+         return localColour;
+      }
+   }
+   else
+   {
+      return scene->background;
+   }
+}
+
+bool
+shootShadowRay(Ray& r)
+{
+   Vector p = r.intersection;
+   Vector l = (scene->light.pos - p).normalise();
+
+   p += l * 0.01;
+   Ray shadowRay(p, l);
+
+   return scene->testIntersection(shadowRay);
+}
+
 void
 teardown()
 {
    delete scene;
-
    delete[] image;
 }
 
@@ -443,25 +321,6 @@ writeImage()
       exit(-1);
    }
    png_write_end(png, NULL);
-   
+
    fclose(outFile);
-}
-
-int
-main(int argc, char** argv)
-{
-   parseArguments(argc, argv);
-
-   scene = new Scene();
-   loadNFFFile();
-   
-   setup();
-
-   castRays();
-   
-   writeImage();
-
-   teardown();
-
-   return 0;
 }
---
+++ Main.h
@@ -0,0 +1,160 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <algorithm>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+
+#include <pthread.h>
+#include <png.h>
+
+#include "AFF/parse.h"
+
+#include "Camera.h"
+#include "Colour.h"
+#include "Light.h"
+#include "Material.h"
+#include "Plane.h"
+#include "Scene.h"
+#include "Sphere.h"
+#include "Vector.h"
+
+using namespace std;
+
+/** The scene. */
+Scene* scene;
+
+/** Title of the window. */
+const char* title = "Straylight";
+/** Whether to only cast rays and produce no output. */
+bool noOutput = false;
+/** The name of the NFF file to parse. */
+char nffFileName[255] = "";
+/** Width of the image to produce. */
+int width = 0;
+/** Height of the image to produce. */
+int height = width;
+/** Amount of threads to spin up. */
+int threadCount = 4;
+
+/** Name of the output file. */
+const char* outFileName = "out.png";
+/** Output file handle. */
+FILE* outFile;
+
+/** Output file bit depth. */
+const int BIT_DEPTH = 8;
+/** Colour type of the output file. */
+const int COLOUR_TYPE = PNG_COLOR_TYPE_RGB;
+/** Interlacing to use for the output file. */
+const int INTERLACE_TYPE = PNG_INTERLACE_NONE; //_ADAM7;
+/** Compression type to use for the output file. */
+const int COMPRESSION_TYPE = PNG_COMPRESSION_TYPE_DEFAULT;
+/** Filtering method to use for the output file. */
+const int FILTER_METHOD = PNG_FILTER_TYPE_DEFAULT;
+/** Colours per pixel in the image. */
+const int COLOURS_PER_PIXEL = 3;
+/** Image data, stored as columns of pixels with three eight-bit values. */
+unsigned char* image;
+
+/** libPNG master structure. */
+png_structp png;
+/** libPNG info structure. */
+png_infop pngInfo;
+
+/**
+ * The main loop for the program.
+ *
+ * Casts rays in turns and writes them to the image buffer and
+ * optionally to the screen.
+ *
+ * @param interactive If true, the image will be output to the
+ * screen while it is being calculated.
+ */
+void
+castRays();
+
+/**
+ * Casts a subset of rays. Used to divide the scene into equal parts for
+ * threading.
+ *
+ * @param arg A single argument that determines which segment of the
+ * scene to render in this thread.
+ *
+ * The scene is divided into threadCount y-parts: cutoff = height
+ * / threadCount, and then the arg is used to determine the lower and
+ * upper bounds: low = arg * cutoff, high = (arg + 1) * cutoff.
+ *
+ * Therefore: arg e N [0; threadCount)
+ */
+void*
+castRaySubset(void* args);
+
+/**
+ * Load the NFF file specified by nffFileName into the scene object.
+ */
+void
+loadNFFFile();
+
+/**
+ * Parse command-line arguments and initialize global variables from them.
+ *
+ * @param argc The amount of command-line arguments provided.
+ * @param argv An array containing the command-line arguments.
+ */
+void
+parseArguments(int argc, char** argv);
+
+/**
+ * Print usage information for the program.
+ */
+void
+printUsage();
+
+/**
+ * Initialize libPNG, allocate global memory and open file handles.
+ *
+ * This function must perform any action that would force the program to
+ * terminate because it is executed before the ray trace operation, which could
+ * take some time.
+ */
+void
+setup();
+
+/**
+ * Shoots an individual ray.
+ *
+ * @param r The ray to shoot.
+ * @return The colour returned by the ray.
+ */
+Colour
+shootRay(Ray& r);
+
+/**
+ * Shoots a ray to determine if an intersection point struck by
+ * a ray is in shadow.
+ *
+ * @return True if the intersection point is in shadow, false
+ * otherwise.
+ */
+bool
+shootShadowRay(Ray& r);
+
+/**
+ * Free any global memory, close file handles and perform any cleanup that must
+ * be done before the program terminates.
+ */
+void
+teardown();
+
+/**
+ * Call libPNG and write the ray traced image to the file specified by
+ * outFileName.
+ */
+void
+writeImage();
+
+#endif
